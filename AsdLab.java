e73267faf45f283eb8672fb88adca6a306e28342
Anton Chaban <a.chaban-2027@kpi.ua>
2020-10-02 00:04:30 +0300

    Покоментил

--- AsdLab.java
+++ AsdLab.java
@@ -1,28 +1,27 @@
-import java.util.Scanner;
+import java.util.Scanner; // подключим сканер
 
 public class AsdLab {
     public static void main(String[] args){
-        Scanner line = new Scanner(System.in);
-        System.out.print("X: ");
+        Scanner line = new Scanner(System.in); 
+        System.out.print("X bin: "); // в 6 и 8 строках попросим ввести значения Х и У в бинарном коде
         String x = line.next();
-        System.out.print("Y: ");
+        System.out.print("Y bin: "); 
         String y = line.next();
-        char[] arrayX = x.toCharArray();
+        char[] arrayX = x.toCharArray(); // переведем полученые строки в массив символов 
         char[] arrayY = y.toCharArray();
         int z = 0;
         long ResultX = 0;
         long ResultY = 0;
 
-        System.out.println(x);
 
 
-        for (int i = arrayX.length-1;i >= 0; i--)
-        {
-            ResultX = ResultX + Character.getNumericValue(arrayX[i]) * Power(2, z);
-            z++;
+        for (int i = arrayX.length-1;i >= 0; i--) // создадим цикл перевода в десятичную систему счисления(начиная с последнего элемента массива)
+        { // длинна-1 необходима т.к первое значения массива 0
+            ResultX = ResultX + Character.getNumericValue(arrayX[i]) * Power(2, z); // переведем символы в цифры (т.к при считывании строки программа рассматривала их как символы и присваивала значения исходя из ASCII таблицы)
+            z++; // увеличиваем степень и гоним цикл
         }
-        z = 0;
-        for (int i = arrayY.length-1;i >= 0; i--)
+        z = 0; // вне цикла уже обнулим степень
+        for (int i = arrayY.length-1;i >= 0; i--) // такие же действия но с массивом У
         {
             ResultY = ResultY + Character.getNumericValue(arrayY[i]) * Power(2, z);
             z++;
@@ -33,20 +32,20 @@ public class AsdLab {
 
 
 
-        if (ResultX % ResultY == 0)
+        if (ResultX % ResultY == 0) // если остаток от деления Х на У равняется 0
         {
-            System.out.print("Ділиться БЕЗ остачі");
+            System.out.print("Ділиться БЕЗ остачі"); 
         }
         else
             {
-                System.out.print("Ділиться З остачею");
+                System.out.print("Ділиться З остачею"); // ну а если не равняется то выводим это
         }
 
 
 
 
     }
-    public static int Power(int x, int n)
+    public static int Power(int x, int n) // создадим функцию поднесения в степень при использовании рекурсии
     {
         if (n == 0) return 1;
 
