9afdd7c7b605e868cda95a19365c543beec0e17b
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2020-11-25 16:05:17 +0200

    Update ASDLab3.java

--- ASDLab3.java
+++ ASDLab3.java
@@ -1,36 +1,42 @@
-public class ASDLab3 {
-    public static void main(String[] args) {
-        double[] testArr = CreateArray();
-        double[] firstCopy = testArr.clone();
-        double[] secondCopy = testArr.clone();
+import java.util.Arrays;
 
-        //System.out.println("//");
-        InsertionSort(testArr);
-        //System.out.println("//");
-        BubbleSort(firstCopy);
-        //System.out.println("//");
-        SelectionSort(secondCopy);
-        //PrintArr(testArr);
-        InsertionSort(CrSortedArr());
-        //System.out.println("//");
-        BubbleSort(CrSortedArr());
-        //System.out.println("//");
-        SelectionSort(CrSortedArr());
+public class AsdLab3 {
+    public static void main(String[] args) {
+        int[] testArr = CreateArray();
+        int[] firstCopy = testArr.clone();
+        int[] arr = CrSortedArr();
+        QuickSort qs = new QuickSort();
+        MergeSort ms = new MergeSort();
+        long m = System.currentTimeMillis();
+        qs.quickSort(testArr, 0, testArr.length-1);
+        System.out.println("QuickSort Time:"+ (double) (System.currentTimeMillis() - m) + "ms");
+        m = System.currentTimeMillis();
+        ms.mergeSort(firstCopy, 0, firstCopy.length-1);
+        System.out.println("MergeSort Time:"+ (double) (System.currentTimeMillis() - m) + "ms");
+        System.out.println(" ");
+        m = System.currentTimeMillis();
+        qs.quickSort(arr, 0, arr.length-1);
+        System.out.println("QuickSort Sorted Time:"+ (double) (System.currentTimeMillis() - m) + "ms");
+        m = System.currentTimeMillis();
+        ms.mergeSort(arr, 0, arr.length-1);
+        System.out.println("MergeSort Sorted Time:"+ (double) (System.currentTimeMillis() - m) + "ms");
+
+
     }
-    private static void PrintArr(double[] array)
+    private static void PrintArr(int[] array)
     {
         for (int i = 0; i < array.length; i++)
         {
-            double x = array[i];
-            System.out.printf("%.2f  ", x);
+            int x = array[i];
+            System.out.print(x + " ");
         }
         System.out.println(" ");
     }
 
-    private static double[] CrSortedArr()
+    private static int[] CrSortedArr()
     {
-        int n = 10000;
-        double[] array = new double[n];
+        int n = 1_000_000;
+        int[] array = new int[n];
         for (int i = 0; i < n; i++)
         {
             array[i] = i+1;
@@ -38,19 +44,127 @@ public class ASDLab3 {
         return array;
     }
 
-    private static double[] CreateArray()
+    public static int[] CreateArray()
     {
-        int n = 10000;
-        double[] array = new double[n];
+        int n = 1_000_000;
+        int[] array = new int[n];
         for (int i = 0; i < n; i++)
         {
-            array[i] = Math.random() * (100 - (-100) + 1) + (-100);
+            array[i] = (int) (Math.random() * (1000 - (-1000) + 1) + (-1000));
         }
         return array;
     }
 
 
-    private static double[] BubbleSort(double[] array)
+    static class QuickSort {
+        private void quickSort(int[] array, int lowest, int highest) {
+            if (lowest < highest) {
+                int divIdx = partition(array, lowest, highest);
+                quickSort(array, lowest, divIdx - 1);
+                quickSort(array, divIdx, highest);
+            }
+        }
+
+        private int partition(int[] array, int lowest, int highest) {
+            int rightIdx = highest;
+            int leftIdx = lowest;
+
+            int pivot = array[lowest + (highest - lowest) / 2];
+            while (leftIdx <= rightIdx) {
+                while (array[leftIdx] < pivot) {
+                    leftIdx++;
+                }
+
+                while (array[rightIdx] > pivot) {
+                    rightIdx--;
+                }
+
+                if (leftIdx <= rightIdx) {
+                    swap(array, rightIdx, leftIdx);
+                    leftIdx++;
+                    rightIdx--;
+                }
+
+            }
+            return leftIdx;
+        }
+
+        private static void swap(int[] array, int index1, int index2) {
+            int tmp  = array[index1];
+            array[index1] = array[index2];
+            array[index2] = tmp;
+        }
+
+    }
+
+
+
+
+    static class MergeSort {
+        private void mergeSort(int[] array, int left, int right) {
+            if (right > left) {
+                int middle = (left + right) / 2;
+                mergeSort(array, left, middle);
+                mergeSort(array, middle + 1, right);
+                Merging(array, left, middle, right);
+            }
+        }
+
+        private void Merging(int[] array, int left, int middle, int right)
+        {
+            int sizeL = middle - left + 1;
+            int sizeR = right - middle;
+            int[] leftArr = new int[sizeL];
+            int[] rightArr = new int[sizeR];
+
+            for (int i = 0; i < sizeL; ++i) // заполняем массив левыми элементами (до центрального элемента)
+            {
+                leftArr[i] = array[left+i];
+            }
+
+            for (int j = 0; j < sizeR; ++j) // заполняем массив правыми элементами (теми что после центрального)
+            {
+                rightArr[j] = array[middle+1+j];
+            }
+
+            int i = 0;
+            int j = 0;
+            int k = left;
+
+            while (i < sizeL && j < sizeR)
+            {
+                if (leftArr[i] <= rightArr[j])
+                {
+                    array[k] = leftArr[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArr[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < sizeL)
+            {
+                array[k] = leftArr[i];
+                i++;
+                k++;
+            }
+
+            while (j < sizeR)
+            {
+                array[k] = rightArr[j];
+                j++;
+                k++;
+            }
+        }
+    }
+
+
+
+/*    private static double[] BubbleSort(double[] array)
     {
         long m = System.nanoTime();
         for (int j = 0; j < array.length-1; j++)
@@ -67,9 +181,9 @@ public class ASDLab3 {
         }
         System.out.println("Bubble Time:"+ (double) (System.nanoTime() - m));
         return array;
-    }
+    }*/
 
-    private static double[] SelectionSort(double[] array)
+/*    private static double[] SelectionSort(double[] array)
     {
         long m = System.nanoTime();
         for (int i = 0; i < array.length-1; i++) {
@@ -85,23 +199,23 @@ public class ASDLab3 {
         }
         System.out.println("Selection Time:"+ (double) (System.nanoTime() - m));
         return array;
-    }
+    }*/
 
-    private static double[] InsertionSort(double[] array)
+/*    private static int[] InsertionSort(int[] array)
     {
-        long m = System.nanoTime();
+        long m = System.currentTimeMillis();
         for (int i = 1; i < array.length-1; i++)
         {
             for (int j=i-1; j > 0 && array[j-1]>array[j]; j--)
             {
-                    double tmp = array[j-1];
+                    int tmp = array[j-1];
                     array[j-1] = array[j];
                     array[j] = tmp;
             }
         }
-        System.out.println("Insertion Time:"+ (double) (System.nanoTime() - m));
+        System.out.println("Insertion Time:"+ (double) (System.currentTimeMillis() - m) + "ms");
 
 return array;
-    }
+    }*/
 
 }
